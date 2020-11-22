7058a1880afba2cf549491e0f706a86806944fac
Vesna Tan <tanvezi@gmail.com>
2020-11-22 13:50:56 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloProvider } from "react-apollo";
+import { HttpLink } from "apollo-link-http";
+const client = new ApolloClient({
+	cache: new InMemoryCache(),
+	link: new HttpLink({
+		uri: `https://1igfg.sse.codesandbox.io/`,
+	}),
+});
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+	<React.StrictMode>
+		<ApolloProvider client={client}>
+			<App />
+		</ApolloProvider>
+	</React.StrictMode>,
+	rootElement
+);
